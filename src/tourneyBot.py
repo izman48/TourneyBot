78e4d4bcf5d3503e1e6f9973442eab426744dd8d
Iesa Wazeer <iesa.wazeer7@gmail.com>
2024-02-10 04:19:14 +0000

    adding reactions affecting messages

--- src/tourneyBot.py
+++ src/tourneyBot.py
@@ -10,10 +10,39 @@ class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.bot_id = ""
+        self.my_emojis = ["🔁", "✅"]
+        self.current_team_message = None
 
     async def on_ready(self):
         print(f"Logged on as {self.user}!")
         self.bot_id = self.user.id
+
+    async def on_reaction_add(self, reaction, user):
+        if user == self.user:
+            return
+        if (
+            reaction.emoji == "🔁"
+            and self.current_team_message is not None
+            and reaction.message.id == self.current_team_message.id
+        ):
+            await self.current_team_message.delete()
+            teams = tournamentCreator(self.players)
+            teams_message = "\n".join(
+                [f"Team {i+1}: {' '.join(players)}" for i, players in enumerate(teams)]
+            )
+
+            created_message = await reaction.message.channel.send(
+                f"```{teams_message}```"
+            )
+            self.current_team_message = created_message
+            for emoji in self.my_emojis:
+                await created_message.add_reaction(emoji)
+        if (
+            reaction.emoji == "✅"
+            and self.current_team_message is not None
+            and reaction.message.id == self.current_team_message.id
+        ):
+            self.current_team_message = None
 
     async def on_message(self, message):
         if message.author == self.user:
@@ -28,24 +57,22 @@ class MyClient(discord.Client):
                         "```I'm a bot that can help you create teams for a tournament if you have more than 8 people. Just type @tourney create in a voice channel and I'll take care of the rest.```"
                     )
                 elif words[1] == "create":
-                    # self.players = [
-                    #     member.name
-                    #     for member in message.author.voice.channel.members
-                    #     if message.author.voice.channel
-                    # ]
                     self.players = [
-                        "Player1",
-                        "Player2",
-                        "Player3",
-                        "Player4",
-                        "Player5",
-                        "Player6",
-                        "Player7",
-                        "Player8",
-                        "Player9",
-                        "Player10",
-                        "Player11",
+                        member.name
+                        for member in message.author.voice.channel.members
+                        if message.author.voice.channel
                     ]
+                    # Use this for testing
+                    # self.players = [
+                    #     "Player1",
+                    #     "Player2",
+                    #     "Player3",
+                    #     "Player4",
+                    #     "Player5",
+                    #     "Player6",
+                    #     "Player7",
+                    #     "Player8",
+                    # ]
                     try:
                         teams = tournamentCreator(self.players)
                         teams_message = "\n".join(
@@ -54,7 +81,13 @@ class MyClient(discord.Client):
                                 for i, players in enumerate(teams)
                             ]
                         )
-                        await message.channel.send(f"```{teams_message}```")
+
+                        created_message = await message.channel.send(
+                            f"```{teams_message}```"
+                        )
+                        self.current_team_message = created_message
+                        for emoji in self.my_emojis:
+                            await created_message.add_reaction(emoji)
                     except InvalidTournamentException as e:
                         await message.channel.send(f"```Error: {e}```")
         print(f"Message from {message.author}: {message.content}")
